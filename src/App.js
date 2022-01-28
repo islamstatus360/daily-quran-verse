6e6ff294fef2a1338e0b86ddf9c332b389689def
Shaheryar Akbar <sheryrao.sa@gmail.com>
2022-01-28 19:07:27 +0500

    fetch verse
    using onclick button we generate a random number and put into api and get value from the api and finally show on web page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+
+  const [val, setVal] = useState();
+  const [snum, setSnum] = useState();
+  const [num, setNum] = useState();
+
+  var min = 1;
+  var max = 6236;
+
+  const getData = async () => {
+    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min; 
+    const result = await fetch(`https://api.alquran.cloud/v1/ayah/${randomNumber}/en.asad`);
+    const mainData = await result.json();
+    console.log(mainData);
+    setVal(mainData.data.text);
+    setSnum(mainData.data.surah.number);
+    setNum(mainData.data.numberInSurah);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper d-flex  align-items-center justify-content-center">
+            <div className="col-6 box p-4 rounded" id="quote-box">
+                    <div className="mb-4">
+                        <p id="text" className="quote"><strong><i className="fas fa-quote-left fa-2x"></i>{val}</strong></p>
+                        
+                        <div className = "writer" >
+                          <cite className="d-block text-center" id="author" className="author">
+                            <h2>[{snum}:{num}]</h2>
+                          </cite>
+                        </div>                        
+                    </div>
+    
+                <div className="d-flex justify-content-between buttons">
+                    <a className="btn btn-primary" target="_blank" href = "#" id="tweet-quote"><i className="fab fa-twitter"></i> Tweet</a>
+                    <button className="btn  btn-outline-primary" onClick={getData} id="new-quote">
+                      Get Next
+                    </button>
+                </div>
+            </div>
+       </div>
+    </>
   );
 }
 
