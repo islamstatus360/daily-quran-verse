d31ef7d38097d6b71a8f95f26b5e27553a0b6f2a
Shaheryar Akbar <sheryrao.sa@gmail.com>
2022-01-30 11:57:19 +0500

    Add Arabic Language
    We can add Arabic language with urdu & English

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [val, setVal] = useState('In the name of God, The Most Gracious, The Dispenser of Grace:');
   const [snum, setSnum] = useState(1);
   const [num, setNum] = useState(1);
-  const [lang, setlang] = useState('en');
+  const [lang, setlang] = useState('en.asad');
 
   var min = 1;
   var max = 6236;
@@ -28,9 +28,9 @@ const App = () => {
 
   const getData = async () => {
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min; 
-    const result = await fetch(`https://api.alquran.cloud/v1/ayah/${randomNumber}/${lang}.asad`);
+    const result = await fetch(`https://api.alquran.cloud/v1/ayah/${randomNumber}/${lang}`);
     const mainData = await result.json();
-    console.log(mainData);
+    console.log(result);
     setVal(mainData.data.text);
     setSnum(mainData.data.surah.number);
     setNum(mainData.data.numberInSurah);
@@ -39,14 +39,18 @@ const App = () => {
   
   const getLanguage = () => {
     let val = lang;
-    if(val === 'en'){
+    if(val === 'en.asad'){
       document.getElementById("text").style.direction = "rtl";
       document.getElementById("language-title").textContent = "Urdu ";
+      setlang('ur.junagarhi');
+    }else if(val === 'ur.junagarhi'){
+      document.getElementById("text").style.direction = "rtl";
+      document.getElementById("language-title").textContent = "Arabic ";
       setlang('ur');
-    }else {
+    }else{
       document.getElementById("text").style.direction = "ltr";
       document.getElementById("language-title").textContent = "English ";
-      setlang('en');
+      setlang('en.asad');
     }
   }
 
